14e8cdc9374bfd46ed7b7e3e4bfcdb3decccc136
Raj123-dev <134774790+Raj123-dev@users.noreply.github.com>
2025-05-04 20:44:28 +0530

    Update HelloWorld.java

--- src/java/com/mycode/HelloWorld.java
+++ src/java/com/mycode/HelloWorld.java
@@ -4,4 +4,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+      public String getMessage() {
+        return "Hello World!";
+    }
 }
