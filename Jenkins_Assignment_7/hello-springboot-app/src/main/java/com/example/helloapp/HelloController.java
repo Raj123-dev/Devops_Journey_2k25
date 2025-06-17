1bbbf6555a576fa3ad7213142a3f88b1028cd9dd
Raj123-dev <134774790+Raj123-dev@users.noreply.github.com>
2025-06-17 15:45:33 +0530

    Update HelloController.java

--- Jenkins_Assignment_7/hello-springboot-app/src/main/java/com/example/helloapp/HelloController.java
+++ Jenkins_Assignment_7/hello-springboot-app/src/main/java/com/example/helloapp/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/hello")
     public String sayHello() {
-        return "Heyyy! That's my Java application.";
+        return "Heyyy Raj! That's my Java application.";
     }
 }
