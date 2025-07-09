00e5077066896630a6c6ce8c131d61098d169de4
Raj Kashyap <raj.kashyap@calsoftinc.com>
2025-07-09 15:31:27 +0530

    codeql

---
+++ Sonar_Assignment_10/hello-springboot-app/src/main/java/com/example/helloapp/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.helloapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hi")
+    public String sayHello() {
+        return "Hi Raj Kashyap!!!!!! That's my Java application.";
+    }
+}
